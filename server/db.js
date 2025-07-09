32067a615c667e612d0278ded6f4388518b4e9d3
sudopatil <rajpravinpatil09@gmail.com>
2025-07-10 00:34:45 +0530

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,7 +1,7 @@
-const mysql = require('mysql2');
+import { createPool } from 'mysql2';
 require('dotenv').config();
 
-const pool = mysql.createPool({
+const pool = createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
@@ -10,4 +10,4 @@ const pool = mysql.createPool({
   connectionLimit: 10,
 });
 
-module.exports = pool.promise();
+export default pool.promise();
